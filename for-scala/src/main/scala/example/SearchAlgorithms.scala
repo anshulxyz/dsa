0f1e38a891c9c1328fe0aa9b5bd33b7d20b65cb9
Anshul Chauhan <anshulchauhan@outlook.com>
2024-07-08 19:49:11 +0530

    Add binary search for Scala

--- for-scala/src/main/scala/example/SearchAlgorithms.scala
+++ for-scala/src/main/scala/example/SearchAlgorithms.scala
@@ -1,17 +1,36 @@
 package example
 
 object SearchAlgorithms{
-  def linearSearch(arr: Array[Int], num: Int): Option[Int] = {
+  def linearSearch(arr: Array[Int], target: Int): Option[Int] = {
     for ((v,k) <- arr.zipWithIndex) {
-      if (v == num) {
+      if (v == target) {
         return Some(k);
-      } else if (v > num) {
+      } else if (v > target) {
         return None;
         // NOTE since we assume that the input array is in sorted order,
         // we don't have to search any further if we encounter a number larger
-        // than the 'num'
+        // than the 'target'
       }
     }
     return None;
+  }
+  def binarySearch(arr: Array[Int], target: Int): Option[Int] = {
+    var lower = 0 // starts at 0th index
+    var upper = arr.length - 1 // I am starting it at the last readable index
+
+    while (lower <= upper) {
+      val pointer = (lower + upper) / 2
+      val mid_value = arr(pointer)
+
+      if (target == mid_value) {
+        return Some(pointer)
+      } else if (target < mid_value) {
+        upper = pointer - 1
+      } else {
+        lower = pointer + 1
+      }
+    }
+
+    None
   }
 }
